0f864a24af18fcad9b334363236ff1c8f99a16bd
wangmiao <zhangrui_19@163.com>
2021-07-06 15:57:33 +0800

    提交项目配置文件

---
+++ settings.py
@@ -0,0 +1,48 @@
+#!/usr/bin/evn python
+# -*- coding: utf-8 -*-
+# @Time     : 2021/7/6 14:25
+# @Author   : dapwn
+# @File     : settings.py
+# @Software : PyCharm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 免费代理网站列表
+# 不翻墙可正常访问的网站：
+FREE_PROXY_URL_0 = 'http://www.kxdaili.com/dailiip.html'                 # 网站：开心代理       ip地区：国内 是否可选位置：否
+FREE_PROXY_URL_1 = 'https://proxy.horocn.com/free-china-proxy/all.html'  # 网站：蜻蜓代理       ip地区：国内 是否可选位置：否
+FREE_PROXY_URL_2 = 'https://proxy.mimvp.com/freeopen'                    # 网站：米扑代理       ip地区：全球 是否可选位置：国内/国外
+FREE_PROXY_URL_3 = 'https://www.proxyscan.io'                            # 网站：ProxyScan     ip地区：全球 是否可选位置：是
+FREE_PROXY_URL_4 = 'https://www.proxy-list.download/HTTP'                # 网站：ProxyList     ip地区：全球 是否可选位置：是
+FREE_PROXY_URL_5 = 'http://proxydb.net'                                  # 网站：ProxyDB       ip地区：全球 是否可选位置：是
+
+# 需翻墙的网站：
+FREE_PROXY_URL_6 = 'http://free-proxy.cz'                                # 网站：FreeProxy     ip地区：全球 是否可选位置：是
+FREE_PROXY_URL_7 = 'https://spys.one'                                    # 网站：spys          ip地区：全球 是否可选位置：是
+FREE_PROXY_URL_8 = 'https://premproxy.com/list'                          # 网站：PremProxy     ip地区：全球 是否可选位置：否
+FREE_PROXY_URL_9 = 'https://free-proxy-list.net'                         # 网站：FreeProxyList ip地区：全球 是否可选位置：是
+FREE_PROXY_URL_10 = 'https://www.proxynova.com/proxy-server-list'        # 网站：ProxyNova     ip地区：全球 是否可选位置：是
